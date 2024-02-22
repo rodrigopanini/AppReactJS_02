db234622a783b6fbec660b219b0dc761bf4bfc23
Rodrigo Cesar Panini <rodrigo_panini@hotmail.com>
2024-02-21 22:16:36 -0300

    Implementação do State

--- lampada-on-off/src/pages/index.js
+++ lampada-on-off/src/pages/index.js
@@ -1,3 +1,35 @@
+import Image from "next/image";
+import { useState } from "react";
+
 export default function Home() {
-  return ( <div></div> );
+
+  const [estadoInterruptor, setEstadoInterruptor] = useState(false)
+  const [contador, setContador] = useState(0)
+
+  function mudarLampada(){
+    setEstadoInterruptor(!estadoInterruptor)
+    if (estadoInterruptor == false)
+      setContador(contador+1)
+  }
+
+  return (
+  <div>
+    <h2>
+      {
+        contador < 3
+          ? `Você já ligou ${contador} vez${contador==1?"" :"es"} a lâmpada`
+          : "Sua lâmpada queimou"
+      }
+    </h2>
+
+    {
+      contador >= 3
+        ? <Image src={"/assets/boom.jpg"} width={286} height={388} /> 
+        : estadoInterruptor == true 
+          ? <Image src={"/assets/on.png"} width={286} height={388} /> 
+          : <Image src={"/assets/off.png"} width={286} height={388} />
+    }
+    <button onClick={mudarLampada}>Interruptor</button>
+  </div>
+  );
 }
